ebd359da2652c3714b1271362f4b5e2cfc20cc4e
Scott Redig <Laremere@gmail.com>
2013-09-11 15:54:31 -0500

    Starting making portals

---
+++ misc/Portal.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+	private static bool renderingPortal = false;
+	
+	public Transform otherPortal;
+	
+	public void OnWillRenderObject(){
+		
+	}
+}
